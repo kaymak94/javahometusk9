9c9a68995d9299297969b6eb38e5c8be425d92c9
Каймакчи Александр <kaymakchiao@mail.ru>
2022-06-02 16:51:36 +0300

    Second commit

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -2,13 +2,13 @@ package ru.netology.domain;
 
 public class Post {
 
-    public String birthday = bithday.FormDate();
+    public FormDate birthday = new FormDate();
 
     public String name;
     public String passport;
     public String patronymic;
     public String phone;
     public String surname;
-    public String subscription;
+    public boolean subscription;
 
 }
